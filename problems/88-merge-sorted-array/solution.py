0ee3d03ec1361011be81dffad6a672e27f4e4aa2
dvdblk <git@dvdblk.com>
2023-09-26 23:38:17 +0200

    initial commit

---
+++ problems/88-merge-sorted-array/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        _nums1 = nums1.copy()
+        i, j = 0, 0
+        for x in range(0, m + n):
+            if j < n and i < m:
+                if nums2[j] > _nums1[i]:
+                    nums1[x] = _nums1[i]
+                    i += 1
+                else:
+                    nums1[x] = nums2[j]
+                    j += 1
+            elif j < n:
+                nums1[x] = nums2[j]
+                j += 1
+            elif i < m:
+                nums1[x] = _nums1[i]
+                i += 1
