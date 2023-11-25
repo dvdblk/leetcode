90b7d6ae4fa2ce52f52a019448db3f50a5a1a61b
dvdblk <github@dvdblk.com>
2023-11-25 19:24:13 +0100

    feat: add 0118 solution.py

---
+++ problems/0118-pascals-triangle/solution.py
@@ -0,0 +1,41 @@
+from typing import List
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        """Recursive solution O(n^2) space & time"""
+        if numRows == 1:
+            # Base case
+            return [[1]]
+        else:
+            # Recursion
+            last_rows = self.generate(numRows-1)
+            last_row = last_rows[-1]
+
+            # Create new row with 1 as pre/suffix
+            new_row = [1]
+            for i in range(len(last_row)-1):
+                # Sum up the previous row values
+                new_row.append(last_row[i] + last_row[i+1])
+
+            new_row.append(1)
+            last_rows.append(new_row)
+            return last_rows
+
+    def generate_2(self, numRows: int) -> List[List[int]]:
+        """Iterative solution O(n^2)"""
+        if numRows == 1:
+            # Base case
+            return [[1]]
+        else:
+            result = [[1]]
+
+            for i in range(numRows-1):
+
+                new_row = [1]
+                for j in range(i):
+                    new_row.append(result[i][j] + result[i][j+1])
+
+                new_row.append(1)
+                result.append(new_row)
+            return result
+
