d6717fc1900c412a78e60b4bd453389635e354b7
dvdblk <git@dvdblk.com>
2023-12-29 23:15:12 +0100

    feat: add 0577 solution.py

---
+++ problems/0577-employee-bonus/solution.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+def employee_bonus(employee: pd.DataFrame, bonus: pd.DataFrame) -> pd.DataFrame:
+    merged_df = pd.merge(employee, bonus, on="empId", how="outer")[["name", "bonus"]]
+    return merged_df[(merged_df["bonus"] < 1000) | merged_df["bonus"].isnull()]
