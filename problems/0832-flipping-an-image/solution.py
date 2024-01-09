ec059fc21c900083e178bd2a60b600e2fd4b2270
dvdblk <github@dvdblk.com>
2024-01-09 13:39:27 +0100

    feat: add 0832 solution.py

--- problems/0832-flipping-an-image/solution.py
+++ problems/0832-flipping-an-image/solution.py
@@ -4,3 +4,16 @@ class Solution:
             image[i] = list(map(lambda n: int(not n), image[i][::-1]))
 
         return image
+
+    def flipAndInvertImage2(self, image: List[List[int]]) -> List[List[int]]:
+        """two pointer"""
+        for i in range(len(image)):
+            left = 0
+            right = len(image[i]) - 1
+            while left <= right:
+                # flip horizontally and invert bits (XOR 1)
+                image[i][left], image[i][right] = image[i][right]^1, image[i][left]^1
+                left += 1
+                right -= 1
+
+        return image
