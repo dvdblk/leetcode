ee1cb87d77729e48d3b909c4cc317222dddcd56e
dvdblk <github@dvdblk.com>
2024-01-26 09:41:04 +0100

    feat: add 0965 solution.py

---
+++ problems/0965-univalued-binary-tree/solution.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node, val):
+            if node is None:
+                return True
+            else:
+                return node.val == val and dfs(node.left, val) and dfs(node.right, val)
+
+        return dfs(root, root.val)
