26b0fe7f56c0e7b6fab93ad4c2cb6634cbcb24ae
David <git@dvdblk.com>
2024-02-06 21:55:19 +0100

    feat: update 0283 solution.py

--- problems/0283-move-zeroes/solution.py
+++ problems/0283-move-zeroes/solution.py
@@ -1,14 +1,35 @@
+from typing import List
+
+
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i, pos = 0, 0
+        # keep track of the zero index that should be swapped
+        zero_index = None
 
-        while i < len(nums):
-            if nums[pos] == 0:
-                nums.pop(pos)
-                nums.append(0)
-            else:
-                pos += 1
-            i += 1
+        for i, num in enumerate(nums):
+            if num == 0:
+                if zero_index is None:
+                    zero_index = i
+                else:
+                    zero_index = min(i, zero_index)
+            elif zero_index is not None:
+                nums[i], nums[zero_index] = nums[zero_index], nums[i]
+                zero_index = min(i, zero_index + 1)
+
+    def moveZeroes2(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # place non-zero at the start of the list
+        j = 0
+        for num in nums:
+            if num != 0:
+                nums[j] = num
+                j += 1
+
+        # fill the rest with zeros
+        for i in range(j, len(nums)):
+            nums[i] = 0
