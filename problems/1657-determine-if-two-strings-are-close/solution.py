b62500791d6591fb49186965d45d310c5866aa93
dvdblk <git@dvdblk.com>
2024-02-07 16:27:31 +0100

    feat: add 1657 solution.py

---
+++ problems/1657-determine-if-two-strings-are-close/solution.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        c1, c2 = Counter(word1), Counter(word2)
+        return list(sorted(c1.values())) == list(sorted(c2.values())) and set(
+            c1.keys()
+        ) == set(c2.keys())
