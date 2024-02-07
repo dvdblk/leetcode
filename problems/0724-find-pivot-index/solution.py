6ed16bc3f125384a968b5fe25715e08555bdc49a
dvdblk <git@dvdblk.com>
2024-02-07 16:01:12 +0100

    feat: update 0724 solution.py

--- problems/0724-find-pivot-index/solution.py
+++ problems/0724-find-pivot-index/solution.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         left = 0
@@ -7,5 +10,32 @@ class Solution:
                 return i
             left += num
             right -= num
+
+        return -1
+
+    def pivotIndex2(self, nums: List[int]) -> int:
+        right_prefix_sum = [0] * (len(nums) + 1)
+
+        for i in range(len(nums) - 1, -1, -1):
+            right_prefix_sum[i] = right_prefix_sum[i + 1] + nums[i]
+
+        left_prefix_sum = 0
+        for i in range(len(nums)):
+            if right_prefix_sum[i + 1] == left_prefix_sum:
+                return i
+
+            left_prefix_sum += nums[i]
+
+        return -1
+
+    def pivotIndex3(self, nums: List[int]) -> int:
+        total_sum = sum(nums)
+        left_prefix_sum = 0
+
+        for i in range(len(nums)):
+            if left_prefix_sum == total_sum - nums[i] - left_prefix_sum:
+                return i
+
+            left_prefix_sum += nums[i]
 
         return -1
