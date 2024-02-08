c0845f0f467012940c189af094930bd89160a07e
David <git@dvdblk.com>
2024-02-08 14:38:16 +0100

    feat: update 0872 solution.py

--- problems/0872-leaf-similar-trees/solution.py
+++ problems/0872-leaf-similar-trees/solution.py
@@ -1,12 +1,18 @@
+from typing import Optional
+
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         """O(n+m)"""
+
         def leaf_vals(node):
             if node is None:
                 return []
