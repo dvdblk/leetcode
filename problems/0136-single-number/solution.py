605a34badb31c288676d4a576b5263b5751ff8c2
David <git@dvdblk.com>
2024-02-09 20:27:42 +0100

    feat: update 0136 solution.py

--- problems/0136-single-number/solution.py
+++ problems/0136-single-number/solution.py
@@ -1,8 +1,10 @@
+from typing import List
+
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        single_nr = nums[0]
-
-        for i in range(1, len(nums)):
-            single_nr ^= nums[i]
-
-        return single_nr
+        """XOR"""
+        res = 0
+        for num in nums:
+            res ^= num
+        return res
