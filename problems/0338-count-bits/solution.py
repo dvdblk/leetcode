aeab49e43509a812ac561062420dcdb4e3f4619c
David <git@dvdblk.com>
2024-02-10 21:38:55 +0100

    feat: update 0338 solution.py

--- problems/0338-count-bits/solution.py
+++ problems/0338-count-bits/solution.py
@@ -1,7 +1,10 @@
+from typing import List
+
+
 class Solution:
     def countBits(self, n: int) -> List[int]:
         res = []
-        for i in range(n+1):
+        for i in range(n + 1):
             res.append(i.bit_count())
 
         return res
@@ -9,7 +12,7 @@ class Solution:
     def countBits2(self, n: int) -> List[int]:
         """Using bitwise operations"""
         res = []
-        for i in range(n+1):
+        for i in range(n + 1):
             b = i
             n = 0
             while b:
@@ -18,3 +21,10 @@ class Solution:
             res.append(n)
 
         return res
+
+    def countBits3(self, n: int) -> List[int]:
+        """Bitwise ops"""
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            dp[i] = dp[i >> 1] + (i & 1)
+        return dp
