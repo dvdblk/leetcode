fd8c6d0db917229740cd367af0ba295807b3ec59
dvdblk <git@dvdblk.com>
2024-05-20 18:15:43 +0200

    feat: add 1863 solution.py

---
+++ problems/1863-sum-of-all-subset-xor-totals/solution.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        """Naive O(n^2)"""
+        superset = [[]]
+        for n in nums:
+            superset += [sub + [n] for sub in superset]
+
+        total_sum = 0
+        for s in superset:
+            subset_xor = 0
+            for si in s:
+                subset_xor ^= si
+
+            total_sum += subset_xor
+        return total_sum
