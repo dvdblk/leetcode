b2089fc91f3ba0af06f041bf19f2775b9a68bea9
dvdblk <git@dvdblk.com>
2024-06-01 08:17:47 +0200

    feat: add 3142 solution.py

---
+++ problems/3142-check-if-grid-satisfies-conditions/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def satisfiesConditions(self, grid: List[List[int]]) -> bool:
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if i + 1 < len(grid) and grid[i][j] != grid[i + 1][j]:
+                    return False
+                if j + 1 < len(grid[i]) and grid[i][j] == grid[i][j + 1]:
+                    return False
+
+        return True
