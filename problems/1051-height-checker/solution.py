f3585e01d690c50795ff16af49d234b09ed9e055
dvdblk <git@dvdblk.com>
2024-06-11 01:01:26 +0200

    feat: add 1051 solution.py

---
+++ problems/1051-height-checker/solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+
+        res = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                res += 1
+
+        return res
